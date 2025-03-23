8f35f064597a655b8b7609ee4054f4a87110cc97
David Cui <112504777+boncui@users.noreply.github.com>
2025-03-23 16:59:25 -0500

    reset2

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 // next.config.js
 
-=======
->>>>>>> f5cd5c3e5f6e0289000d9ac1e8a67a07c901ab15
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-<<<<<<< HEAD
     // only include this if you're using App Router features
     appDir: true,
-=======
     serverActions: true,
->>>>>>> f5cd5c3e5f6e0289000d9ac1e8a67a07c901ab15
   },
 };
 
